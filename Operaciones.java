75547938dd110e2ba16b31b60b1d10420bbd922d
hh <henry.dos@gmail.com>
2018-03-12 00:49:59 -0600

    Estudiante1 crea   la   función   para   multiplicar   y   luego

--- Operaciones.java
+++ Operaciones.java
@@ -1,8 +1,17 @@
 
-/*Creanod la Funcion suma*/
+/*Creando la Funcion suma Estudiante1*/
 public int Suma(int param1, int param2)
 {
 int resultado;
 resultado=param1+param2;
 return resultado;
 }
+
+/*Creando la Funcion Multiplicacion Estudiante1*/
+public int Multiplicacion(int param1, int param2)
+{
+int resultado;
+resultado=param1*param2;
+return resultado;
+}
+
