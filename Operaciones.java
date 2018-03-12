1b557b0f9ed652280ad31796e42111b1f925160d
hh <henry.dos@gmail.com>
2018-03-12 00:19:11 -0600

    Estudiante 2 hace   check-out   y   agrega   la   función   resta   al   archivo

---
+++ Operaciones.java
@@ -0,0 +1,15 @@
+/*Creanod la Funcion suma*/
+public int Suma(int param1, int param2)
+{
+int resultado;
+resultado=param1+param2;
+return resultado;
+}
+
+/*Creanod la Funcion Resta Estudiante2*/
+public int Resta(int param1, int param2)
+{
+int resultado;
+resultado=param1-param2;
+return resultado;
+}
